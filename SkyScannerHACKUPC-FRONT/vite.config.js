0d3cebc9ac63393dd3414949b2073660e5470408
Ki-re <erikbatiste10@gmail.com>
2025-05-04 06:03:34 +0200

    Update vite.config.js

--- SkyScannerHACKUPC-FRONT/vite.config.js
+++ SkyScannerHACKUPC-FRONT/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: [
-    'hackupc2025.kire.ovh'
-  ],
+  server: {
+    host: '0.0.0.0',
+
+    allowedHosts: [
+      'hackupc2025.kire.ovh'
+    ],
+
+    // hmr: {
+    //   host: 'hackupc2025.kire.ovh',
+    //   protocol: 'ws'
+    // }
+  }
 })
